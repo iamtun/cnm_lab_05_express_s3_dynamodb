3bc744a6c52b1d2ad6fdbe14f827ece248d345e4
iamtun <letuan19431791.iuh@gmail.com>
2024-03-02 17:53:37 +0700

    handle add product

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 ﻿import express from 'express';
 import AWS from 'aws-sdk';
-
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import e from 'express';
 //constant
 const PORT = process.env.PORT || 3000;
 const TABLE_NAME = process.env.TABLE_NAME || 'Products';
 
+//aws config
 AWS.config.update({
     region: process.env.REGION,
     accessKeyId: process.env.ACCESS_KEY_ID,
@@ -12,6 +15,25 @@ AWS.config.update({
 });
 
 const docClient = new AWS.DynamoDB.DocumentClient();
+
+const s3 = new AWS.S3();
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'lab05-s3-dynamo',
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            cb(
+                null,
+                'uploads/' + Date.now().toString() + '-' + file.originalname,
+            );
+        },
+    }),
+});
+
+//init server
 const app = express();
 
 //register middlewares
@@ -38,8 +60,29 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/products', (req, resp) => {
-    console.log(req.body);
+app.post('/products', upload.single('file'), (req, res) => {
+    const fileUrl = req.file.location;
+    const { product_name, product_type, product_quantity } = req.body;
+
+    const params = {
+        TableName: TABLE_NAME,
+        Item: {
+            id: new Date().getTime().toString(),
+            product_name,
+            product_type,
+            product_quantity,
+            product_image: fileUrl,
+        },
+    };
+
+    docClient.put(params, (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.redirect('/');
+        } else {
+            return res.redirect('/');
+        }
+    });
 });
 
 app.listen(PORT, () => {
