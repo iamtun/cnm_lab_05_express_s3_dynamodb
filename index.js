1922bbb03b21e6af1c5a383abc1d0ff39f121673
iamtun <letuan19431791.iuh@gmail.com>
2024-03-02 16:32:36 +0700

    init view

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-﻿
+﻿import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+//register middlewares
+app.use(express.json({extended: false}));
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('./views'));
+
+//config view
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+// routers
+app.get('/', (req, resp) => {
+    return resp.render('index', {products: []})
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
